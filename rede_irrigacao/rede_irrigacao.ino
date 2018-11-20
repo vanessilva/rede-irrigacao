e41b4e3846bdb0e233f09c3c1feb14cfde072cba
Vanessa Silva <vanessa.apdsilva@outlook.com>
2018-11-20 00:22:22 -0200

    Higrômetro; modularização

--- rede_irrigacao/rede_irrigacao.ino
+++ rede_irrigacao/rede_irrigacao.ino
@@ -1,5 +1,11 @@
 #include <DHT.h>
 
+// ====== PINAGEM ======= //
+// Higrômetro
+// VIN --> VCG
+// GND --> GND
+// A0  --> VP - pino de leitura do higrômetro A0
+#define SOIL_PIN A0
 
 // Sensor de temperatura e umidade do ar
 // Visão frontal do DHT11
@@ -12,15 +18,41 @@
 
 DHT dht(DHTPIN, DHTTYPE);
 
+int lerSensorHigrometro() {
+  int valorLeitura;
+  int valorConvertido;
+  valorLeitura = analogRead(SOIL_PIN);
+  valorConvertido = constrain(valorLeitura, 0, 4095);
+  valorConvertido = map(valorConvertido, 0, 4095, 100, 0);
+  Serial.print("Leitura H: ");
+  Serial.print(valorLeitura);
+  Serial.print(" / Conversao H: ");
+  Serial.println(valorConvertido);
+  return valorConvertido;
+}
+
+int lerSensorUmidadeAr() {
+  int leituraUmidade = dht.readHumidity();
+  return leituraUmidade;
+}
+
+int lerSensorTemperatura() {
+  int leituraTemperatura = dht.readTemperature();
+  return leituraTemperatura;
+}
+
 void setup() {
   Serial.begin(9600);
   dht.begin();
 }
 
 void loop() { 
-  int leituraUmidade = dht.readHumidity();  
-  int leituraTemperatura = dht.readTemperature();
-  Serial.print("Umidade: ");
+  int leituraUmidade = lerSensorUmidadeAr();  
+  int leituraTemperatura = lerSensorTemperatura();
+  int leituraHigrometro = lerSensorHigrometro();
+  Serial.print("Higrômetro: ");
+  Serial.print(leituraHigrometro);
+  Serial.print("% / Umidade: ");
   Serial.print(leituraUmidade);
   Serial.print("% / Temperatura: ");
   Serial.println(leituraTemperatura);
