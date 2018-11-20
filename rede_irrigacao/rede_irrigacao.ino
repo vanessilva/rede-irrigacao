f3b7c831d7520a1e588d448e0e884f3fe49a37a0
Vanessa Silva <vanessa.apdsilva@outlook.com>
2018-11-20 00:16:08 -0200

    Comentarios

--- rede_irrigacao/rede_irrigacao.ino
+++ rede_irrigacao/rede_irrigacao.ino
@@ -1,19 +1,23 @@
 #include <DHT.h>
 
 
+// Sensor de temperatura e umidade do ar
+// Visão frontal do DHT11
+// 1 --> 3V3
+// 2 --> D23
+// 3 -->
+// 4 --> GND
 #define DHTPIN 23
 #define DHTTYPE DHT11
 
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   dht.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:  
+void loop() { 
   int leituraUmidade = dht.readHumidity();  
   int leituraTemperatura = dht.readTemperature();
   Serial.print("Umidade: ");
